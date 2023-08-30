5f766e47f554c3798eb06c30acae88f56a3f4017
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-08-30 11:35:10 -0400

    clase 4

--- miAplicacion/src/app/cinco/cinco.page.ts
+++ miAplicacion/src/app/cinco/cinco.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-cinco',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CincoPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
   }
-
+  async alerta()
+  {
+    const alert = await this.alertController.create({
+      header:'Titulo de Alerta',
+      subHeader:"Usando alert controller",
+      message:"Mensaje de prueba con alert controller" ,
+      buttons:['Ok']
+      });
+      await  alert.present(); // muestra el alerta con el mensaje      
+  }
 }
