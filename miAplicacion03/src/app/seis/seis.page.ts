d50e4da12743d4dc5ca7da416379b11b050b99ed
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-08-31 11:21:50 -0400

    clase 4

--- miAplicacion03/src/app/seis/seis.page.ts
+++ miAplicacion03/src/app/seis/seis.page.ts
@@ -6,8 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seis.page.scss'],
 })
 export class SeisPage implements OnInit {
+  numero : number; // debe inicializar
+  nombre : string;
+  permitir: boolean;
+  algo : any;
+  numero1 : number = 1;
 
-  constructor() { }
+  apellido : string = "Pérez";
+  mensaje = "Alumno con excelencia académica";
+
+  asignaturas = [
+    {nombre:"Base de datos" , nota:39.55},
+    {nombre:"Programación"  , nota:45},
+    {nombre:"Matématica"    , nota:33},    
+  ]
+
+  constructor() { 
+    this.numero = 1;
+    this.nombre = "Ana";
+    this.permitir = true;
+  }
 
   ngOnInit() {
   }
