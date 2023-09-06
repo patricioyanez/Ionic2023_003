a6ae681daf11cde1d179aa33af82da20b3101895
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-09-06 12:02:42 -0300

    Clase 5

--- miAplicacion03/src/app/siete/siete.page.ts
+++ miAplicacion03/src/app/siete/siete.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-siete',
@@ -12,7 +13,7 @@ export class SietePage implements OnInit {
   n1= '';
   n2= '';
   resultado = '';
-  constructor() { }
+  constructor(private toastController: ToastController) { }
 
   ngOnInit() {
   }
@@ -24,5 +25,28 @@ export class SietePage implements OnInit {
   {
     this.resultado = "" + (Number(this.n1) - Number(this.n2));
   }
-
+  multiplicar()
+  {
+    this.resultado = "" + (Number(this.n1) * Number(this.n2));
+  }
+  async dividir()
+  {
+    if(Number(this.n2) != 0)
+      this.resultado = "" + (Number(this.n1) / Number(this.n2));
+    else
+    {
+      const toast = await this.toastController.create({
+        message: 'No se puede dividir por cero' ,
+        duration: 3000,
+        position:"middle", // top,bottom
+        color: "danger",
+      });
+      await toast.present();
+    }
+  }
+  
+  // Ejercicios:
+  // crear una nueva pagina que permita calculador el promedio de 3 notas,
+  // mostrar si aprobó o no, según resultado obtenido.
+  // usar alert si reprueba y toast si aprueba.
 }
