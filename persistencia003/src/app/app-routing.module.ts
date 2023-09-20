58c708ff507154e589830777b11fd30839b422b0
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-09-20 12:12:37 -0300

    pagina2

--- persistencia003/src/app/app-routing.module.ts
+++ persistencia003/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'segunda/:valorLink',
+    loadChildren: () => import('./segunda/segunda.module').then( m => m.SegundaPageModule)
   },
   {
     path: 'segunda',
